6010f258e9d51e40e5ed1c9961e1a73998272abb
elshad <elshadalizade111@gmail.com>
2023-12-03 15:03:08 +0400

    fixing

--- main/food/models.py
+++ main/food/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=200)
-    description = models.TextField(max_length=300)
+    description = models.CharField(max_length=300)
     price = models.IntegerField()
     slug = models.SlugField(unique=True)
     image = models.CharField(max_length=500, default='https://static.wixstatic.com/media/bf242e_6133b4ae6a104cc2b50d70179f35efea~mv2.jpg/v1/fill/w_500,h_376,al_c,lg_1,q_80,enc_auto/food-placeholder.jpg')
--- main/food/urls.py
+++ main/food/urls.py
@@ -3,7 +3,7 @@ from .views import update_item, delete_item, CreateItem, DetailItem, IndexClassV
 
 
 urlpatterns = [
-    path('food/', IndexClassView.as_view(), name='index'),
+    path('', IndexClassView.as_view(), name='index'),
     path('food/<slug:slug>', DetailItem.as_view(), name='detail'),
     path('create-item/', CreateItem.as_view(), name='create-item'),
     path('update-item/<slug:item_slug>', update_item, name='update-item'),
